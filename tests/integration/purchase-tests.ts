3676579a3f0763d7314500492f1acff27ed6a641
guilhermeicarofr <guilhermeicarofr@gmail.com>
2023-02-16 01:21:11 -0300

    refactor: removed faker lib from project

--- tests/integration/purchase-tests.ts
+++ tests/integration/purchase-tests.ts
@@ -1,6 +1,5 @@
 import supertest from 'supertest';
 import httpStatus from 'http-status';
-import { faker } from '@faker-js/faker';
 
 import { server } from 'index';
 
@@ -9,8 +8,8 @@ const testServer = supertest(server);
 describe('GET /purchase/estimate/user/:userId', () => {
   describe('when request is invalid', () => {
     it('should respond with status 400 BAD REQUEST if userId is a string', async () => {
-      const response = await testServer.get(`/purchase/estimate/user/${faker.datatype.string()}`).send({
-        products: [faker.datatype.number({min: 1})]
+      const response = await testServer.get(`/purchase/estimate/user/string}`).send({
+        products: [ 1, 2, 3 ]
       });
   
       expect(response.status).toBe(httpStatus.BAD_REQUEST);
@@ -18,32 +17,42 @@ describe('GET /purchase/estimate/user/:userId', () => {
 
     it('should respond with status 400 BAD REQUEST if userId is an invalid number', async () => {
       const response = await testServer.get(`/purchase/estimate/user/0`).send({
-        products: [faker.datatype.number({min: 1})]
+        products: [ 1, 2, 3 ]
       });
   
       expect(response.status).toBe(httpStatus.BAD_REQUEST);
     });
 
     it('should respond with status 400 BAD REQUEST if body is empty', async () => {
-      const response = await testServer.get(`/purchase/estimate/user/${faker.datatype.string()}`).send({});
+      const response = await testServer.get(`/purchase/estimate/user/1`).send({});
   
       expect(response.status).toBe(httpStatus.BAD_REQUEST);
     });
 
     it('should respond with status 400 BAD REQUEST if body is invalid', async () => {
-      const response = await testServer.get(`/purchase/estimate/user/${faker.datatype.string()}`).send({
-        products: 'products'
+      const response = await testServer.get(`/purchase/estimate/user/1`).send({
+        products: 'string'
       });
   
       expect(response.status).toBe(httpStatus.BAD_REQUEST);
     });
 
     it('should respond with status 400 BAD REQUEST if body is an invalid list of ids', async () => {
-      const response = await testServer.get(`/purchase/estimate/user/${faker.datatype.string()}`).send({
+      const response = await testServer.get(`/purchase/estimate/user/1`).send({
         products: [ 1, 0, -3 ]
       });
   
       expect(response.status).toBe(httpStatus.BAD_REQUEST);
+    });
+  });
+  
+  describe('when provided Ids are not existent', () => {
+    it('should respond with status 404 NOT FOUND if userId is not existent', async () => {
+      const response = await testServer.get(`/purchase/estimate/user/10000`).send({
+        products: [ 1, 2, 3 ]
+      });
+  
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
     });
   });
 });
